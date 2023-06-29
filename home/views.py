4f54ab6eb9b9eded05ce3ccf9427c4d541fdd018
KhatriSuson <susonkhatri110@gmail.com>
2023-06-29 17:04:57 +0545

    static commit

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import *
 
 
 # Create your views here.
 
+class Base(View):
+    views = {}
 
-class HomeView(View):
+
+
+class HomeView(Base):
     def get(self, request):
-        return render(request, 'index.html')
+        self.views['categories'] = Category.objects.all()
+        self.views['brands'] = Brand.objects.all()
+        return render(request, 'index.html',self.views)
